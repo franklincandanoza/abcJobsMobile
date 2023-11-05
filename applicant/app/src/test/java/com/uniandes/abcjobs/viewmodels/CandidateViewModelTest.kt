9faab964831fb1541f6343097e5d3aec19155bf2
Franklin Candanoza <fcandanoza@Franklins-MacBook-Pro.local>
2023-11-05 16:21:19 -0500

    Add validation test

---
+++ applicant/app/src/test/java/com/uniandes/abcjobs/viewmodels/CandidateViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.uniandes.abcjobs.viewmodels
+import org.junit.Test
+
+class CandidateViewModelTest {
+
+    @Test
+    fun onSuccessCreateAcademicInfoShown() {
+        assert(false)
+    }
+}
