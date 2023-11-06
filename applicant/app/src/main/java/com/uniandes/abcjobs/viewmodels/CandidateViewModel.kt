981d09e7a71d89775b546cd9a0daf788cbcd80ec
Franklin Candanoza <fcandanoza@Franklins-MacBook-Pro.local>
2023-11-05 21:36:50 -0500

    testsonar

--- applicant/app/src/main/java/com/uniandes/abcjobs/viewmodels/CandidateViewModel.kt
+++ applicant/app/src/main/java/com/uniandes/abcjobs/viewmodels/CandidateViewModel.kt
@@ -92,6 +92,21 @@ class CandidateViewModel(application: Application) :  AndroidViewModel(applicati
         }
     }
 
+    private fun refreshCandidates2() {
+        viewModelScope.launch(Dispatchers.Default) {
+            withContext(Dispatchers.IO) {
+                candidatesRepo.refreshData({
+                    candidatesMutableData.postValue(it)
+                    _eventNetworkError.postValue(false)
+                    _isNetworkErrorShown.postValue(false)
+                }, {
+                    Log.d("Error", it.toString())
+                    _eventNetworkError.postValue(true)
+                })
+            }
+        }
+    }
+
     fun createCandidate(request : CandidateRequest): String {
         var responseCode="";
         try {
